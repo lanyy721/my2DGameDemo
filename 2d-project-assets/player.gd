02c520f859bf17d5872367f56f9becaea1a4c961
lanyy721 <lanyy721@users.noreply.github.com>
2026-07-23 14:48:58 +0800

    Add player health and game over flow

--- 2d-project-assets/player.gd
+++ 2d-project-assets/player.gd
@@ -1,4 +1,7 @@
 extends CharacterBody2D
+signal health_depleted
+
+var health=100.0
 
 func _physics_process(_delta: float) -> void:
 	var direction=Input.get_vector("move_left","move_right","move_up","move_down")
@@ -8,4 +11,10 @@ func _physics_process(_delta: float) -> void:
 		get_node("HappyBoo").play_walk_animation()
 	else:
 		get_node("HappyBoo").play_idle_animation()
-		
+	const DAMAGE_RATE=5.0
+	var overlapping_mobs=$%HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size()>0:
+		health-=DAMAGE_RATE*overlapping_mobs.size()*_delta
+		$%ProgressBar.value=health
+		if health<=0.0:
+			health_depleted.emit()
